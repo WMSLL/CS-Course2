f404ca15555b85f29e5170f2f56092706c99f388
Кизлик виктор <62074815+WMSLL@users.noreply.github.com>
2020-03-17 12:04:49 +0300

    Домашняя работа 1

---
+++ LESSONS/2/ClassWork/ConsoleApp1/HOMEWORK1-2/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace HOMEWORK1_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите первое число");
+
+            string a = Console.ReadLine();
+
+            int number1 = int.Parse(a);
+
+            Console.WriteLine("Введите второе число");
+
+            string b = Console.ReadLine();
+
+            int number2 = int.Parse(b);
+
+            Console.WriteLine("Введите тип операции(+,-,/,*,%,^)");
+
+            var t = Console.ReadLine();
+
+            if (t=="+")
+            {
+                Console.WriteLine("Сумма:");
+                Console.WriteLine(number1 + number2);
+            }
+            else
+            if (t=="-")
+            {
+                Console.WriteLine("Разница:");
+                Console.WriteLine(number1 - number2);
+            }
+            else
+            if (t == "/")
+            {
+                Console.WriteLine("деление:");
+                Console.WriteLine(number1 / number2);
+            }
+            else
+            if (t == "%")
+            {
+                Console.WriteLine("Остаток:");
+                Console.WriteLine(number1 % number2);
+            }
+            else
+            if (t == "^")
+            {
+                Console.WriteLine("Степень:");
+                Console.WriteLine(Math.Pow(number1, number2));
+            }
+            else
+            {
+                Console.WriteLine("Вы ввели не верный символ");
+                
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
