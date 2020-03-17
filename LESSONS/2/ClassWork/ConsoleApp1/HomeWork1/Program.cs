7e16b74a1ea7fe00fc102d104fc246743f72d340
Кизлик виктор <62074815+WMSLL@users.noreply.github.com>
2020-03-17 11:40:42 +0300

    Домашняя работа 1

---
+++ LESSONS/2/ClassWork/ConsoleApp1/HomeWork1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace HomeWork1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Введите первое число");
+
+            string a = Console.ReadLine();
+
+            int number1 = int.Parse(a);
+
+            Console.WriteLine("Введите второе число");
+
+            string b = Console.ReadLine();
+
+            int number2 = int.Parse(b);
+
+            Console.WriteLine("Сумма:");
+
+            Console.WriteLine(number1+number2);
+
+            Console.WriteLine("Разница:");
+            Console.WriteLine(number1 - number2);
+
+
+            Console.WriteLine("Произведение:");
+            Console.WriteLine(number1 * number2);
+
+            Console.ReadKey();
+        }
+    }
+}
