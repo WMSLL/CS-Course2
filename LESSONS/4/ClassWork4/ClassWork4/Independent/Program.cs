74feaf635b5cc6e1fb866f64f355aea27183e9fc
Кизлик виктор <62074815+WMSLL@users.noreply.github.com>
2020-03-23 20:47:57 +0300

    Самостоятельная

--- LESSONS/4/ClassWork4/ClassWork4/Independent/Program.cs
+++ LESSONS/4/ClassWork4/ClassWork4/Independent/Program.cs
@@ -10,33 +10,14 @@ namespace Independent
             var height = double.Parse(Console.ReadLine());
             Console.WriteLine("Введите 'a'");
             var a = double.Parse(Console.ReadLine());
-
             var H = Math.Sqrt((height * height) - ((a * a) / 12.0));
-
             // Math.Pow(height.2)  Степень Числа
-
             Console.WriteLine("S Бок");
-
             Console.WriteLine(3.0 * a * height);
-
-
             Console.WriteLine("S Полн");
-
             Console.WriteLine($"{(3.0 / 2.0) * a * (a * Math.Sqrt(3) + 2 * height)}");
-
-
-
             Console.WriteLine("V");
-
             Console.WriteLine($"{(a * a / 2.0) * H * (Math.Sqrt(3))}");
-
-
-
-
-
-
-
-
         }
 
 
