dbc86f3ad96bb7636b9f7809b1300d568cca1f87
Кизлик виктор <62074815+WMSLL@users.noreply.github.com>
2020-03-28 02:15:20 +0300

    Correct HomeWork 5

--- LESSONS/5/HomeWork5/HomeWork5/HomeWork5/Program.cs
+++ LESSONS/5/HomeWork5/HomeWork5/HomeWork5/Program.cs
@@ -10,57 +10,66 @@ namespace HomeWork5
     }
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Выберите фигуру у которой нужно посчитать площадь 'S'(Круг=1,Прямоугольник=2, Треугольник=3)");
-            Square();
-            Console.WriteLine("Нажмити любую клавишу");
-            Console.ReadKey();
+            area();
         }
 
-
-        static void Square()
-        {            
-            var selectFigure = Enum.Parse(typeof(figure), Console.ReadLine().Replace(".", ","));
+        static void area()
+        {
+        returnenum:
+            Console.WriteLine("Выберите фигуру у которой нужно посчитать площадь 'S'(Круг=1,Прямоугольник=2, Треугольник=3)");
             try
             {
+                var selectFigure = Enum.Parse(typeof(figure), Console.ReadLine());
                 switch (selectFigure)
                 {
                     case figure.circle:
-                        Console.WriteLine("Введите Радиус круга");
-                        var radius = double.Parse(Console.ReadLine().Replace(".", ","));
-                        Console.WriteLine($"Площадь круга равна {3.14 * Math.Pow(radius, 2)}");
+                        double radius = ReadParam("Введите радиус круга");
+                        Console.WriteLine($" Площадь круга :{Math.PI * Math.Pow(radius, 2)}");
                         break;
 
                     case figure.rectangle:
-                        Console.WriteLine("Введите 1-ю сторону прямоугольника");
-                        var side1 = double.Parse(Console.ReadLine().Replace(".", ","));
-                        Console.WriteLine("Введите 2-ю сторону прямоугольника");
-                        var side2 = double.Parse(Console.ReadLine().Replace(".", ","));
-                        Console.WriteLine($"Площадь прямоугольника {(side1 * side2)}");
+                        double side1 = ReadParam("Введите сторону 'a' Прямоугольника");
+                        double side2 = ReadParam("Введите сторону 'b' Прямоугольника");
+                        Console.WriteLine($" Площадь круга :{side1 * side2}");
                         break;
 
                     case figure.triangular:
-                        Console.WriteLine("Введите cторону равностороннего треугольника");
-                        var sideTriangular = double.Parse(Console.ReadLine().Replace(".", ","));
-                        Console.WriteLine($"Площадь равностороннего треугольника {(Math.Pow(sideTriangular, 2) * Math.Sqrt(3)) / 4}");
+                        double sideTriangular = ReadParam("Введите сторону равностороннего треугольника");
+                        Console.WriteLine($" Площадь треугольного :{Math.Pow(sideTriangular, 2) * Math.Sqrt(3) / 4}");
                         break;
 
                     default:
-                        Console.WriteLine("Введена не существующая фигура");
-                        Console.WriteLine("Повторите ввод фигуры у которой нужно посчитать площадь 'S'(Круг=1,Прямоугольник=2, Треугольник=3)");
-                        Square();
-                        break;
+                        Console.WriteLine("Ввели не верную фигуру повторите ввод");
+                        goto returnenum;
                 }
             }
-            catch (FormatException)
+            catch (Exception)
             {
-                Console.WriteLine("Ошибка! Введено нечисловое значение!");
-                Console.WriteLine("Повторите ввод фигуры у которой нужно посчитать площадь 'S'(Круг=1,Прямоугольник=2, Треугольник=3)");
-                Square();              
+                Console.WriteLine("Ввели не существующие значение");
+                goto returnenum;
             }
-           
-            
+            Console.WriteLine("Нажмити любую клавишу для завершения");
+            Console.ReadKey();
+        }
+
+        static double ReadParam(string title)
+        {
+            for (; ; )
+            {
+                try
+                {
+                    Console.WriteLine(title);
+                    return double.Parse(Console.ReadLine().Replace(".", ","));
+                    break;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Вы ввели не числовое значение, повторите ввод");
+                }
+
+            }
         }
     }
 }
