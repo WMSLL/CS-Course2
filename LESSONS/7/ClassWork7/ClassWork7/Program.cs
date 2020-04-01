884b949ff27f018630d3b89e441b1e998f14e7ac
Кизлик виктор <62074815+WMSLL@users.noreply.github.com>
2020-04-01 20:14:08 +0300

    Class7

---
+++ LESSONS/7/ClassWork7/ClassWork7/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ClassWork7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите первое число ");
+            var num1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите второе число ");
+            var num2 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Произведение чисел"+ num1+" И " + num2 +" равна"+ (num1*num2).ToString("0.###"));
+
+            Console.WriteLine("Сумма "+(num1+num2).ToString("0.###"));
+
+            Console.WriteLine($"Вычитание: {(num1-num2).ToString("0.###")}");
+        }
+
+       
+    }
+}
