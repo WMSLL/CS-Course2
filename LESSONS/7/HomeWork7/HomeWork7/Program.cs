f94a0cfa07d775394b0402a0fa6d4dd37546b64e
Кизлик виктор <62074815+WMSLL@users.noreply.github.com>
2020-04-02 17:25:50 +0300

    HomeWork7-1

---
+++ LESSONS/7/HomeWork7/HomeWork7/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace HomeWork7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sum = 0;
+            while (true)
+            {
+                Console.WriteLine("Введите строку из 2-х и более слов");
+                var text = Console.ReadLine();
+                var spliText = text.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                if (spliText.Length <= 1)
+                {
+                    continue;
+                }
+                foreach (var word in spliText)
+                {
+                    if (word.ToUpper().Substring(0, 1) == "А")
+                    {
+                        sum++;
+                    }
+                }
+                Console.WriteLine($"Количество слов начинающихся с буквы ''A'' {sum}");
+                break;
+            }
+        }
+    }
+}
