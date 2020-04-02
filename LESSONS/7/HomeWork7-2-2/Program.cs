e9dad0ba16792d78cebeb1847e7cdef749508f72
Кизлик виктор <62074815+WMSLL@users.noreply.github.com>
2020-04-02 21:14:24 +0300

    HomeWork7-2-2

---
+++ LESSONS/7/HomeWork7-2-2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace HomeWork7_2_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строку из 2-х и более слов");
+            var text = Console.ReadLine().ToLower();
+            var reverseText = "";
+            for (int i = text.Length - 1; i >= 0; i--)
+            {
+                reverseText += text[i];
+            }
+            Console.WriteLine(reverseText);
+            Console.WriteLine("Нажмите любую клавишу для выхода");
+            Console.ReadKey();
+        }
+    }
+}
