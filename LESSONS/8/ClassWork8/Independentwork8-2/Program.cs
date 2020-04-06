32515af148d0c9b41ac27f02dadf2ac8e06c4894
Кизлик виктор <62074815+WMSLL@users.noreply.github.com>
2020-04-06 21:24:17 +0300

    Independentwork8-3

--- LESSONS/8/ClassWork8/Independentwork8-2/Program.cs
+++ LESSONS/8/ClassWork8/Independentwork8-2/Program.cs
@@ -3,11 +3,40 @@ using System.Collections.Generic;
 
 namespace Independentwork8_2
 {
+    
     class Program
     {
         static void Main(string[] args)
         {
-           
+            var dictCountry = new Dictionary<int, string>() { 
+            { 1,"Российская Федерация"},
+            { 2,"США"},
+            { 3,"Германия"},
+            { 4,"Япония"},
+            { 5,"Китай"} };
+
+            var dictСapital = new Dictionary<int, string>() {
+            { 1,"Москва"},
+            { 2,"Вышингтон"},
+            { 3,"Берлин"},
+            { 4,"Токио"},
+            { 5,"Пекин"} };
+
+            var rnm = new Random().Next(1, 5);
+
+            var country = dictCountry[rnm] ;
+
+            Console.WriteLine($"Какая столица у {country}");
+
+            var Сapital=Console.ReadLine();
+
+            Console.WriteLine();
+
+            
+
+
+
+
         }
     }
 }
---
+++ LESSONS/8/ClassWork8/Independentwork8-3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Independentwork8_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var colletion = new Queue<int>();
+            while (true)
+            {
+                Console.WriteLine("Введите числодля очереди");
+               var  str= Console.ReadLine();
+                if (str.ToUpper()=="RUN")
+                {
+                    while (colletion.Count>0)
+                    {
+                        Console.WriteLine($"Выполнилась задая № :{colletion.Dequeue()}");
+                    }
+                    break;
+                }
+                else if (str.ToUpper() == "EXIT")
+                {
+                    Console.WriteLine($"В очереди осталось : {colletion.Count} ");
+                    break;
+                }
+                else
+                {
+                    colletion.Enqueue(int.Parse(str));
+                }
+
+            }
+        }
+
+        
+    }
+}
