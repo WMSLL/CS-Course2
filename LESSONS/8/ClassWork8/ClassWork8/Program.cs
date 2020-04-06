40833c18a0220e439f4c9e55528432f2a1da2cb1
Кизлик виктор <62074815+WMSLL@users.noreply.github.com>
2020-04-06 19:50:23 +0300

    Independentwork8

---
+++ LESSONS/8/ClassWork8/ClassWork8/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+namespace ClassWork8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            
+            
+           // var integer = new List<int>();
+           // integer.Add(10);
+           // integer.Add(20);
+           // integer.Add(3);
+           // integer.Add(22);
+           // integer.Add(54);
+           // integer.Add(21);
+           //
+           // Console.WriteLine(string.Join(",",integer));
+           //
+           //
+           // var stri = new List<string>();
+           // stri.Add("stri");
+           // stri.Add("ss");
+           // stri.Add("sdjdn");
+           // stri.Add("sdjdn");
+           // stri.Add("sdjdn");
+           // stri.Add("sdjdn");
+           //
+           // Console.WriteLine(string.Join(",", stri));
+        }
+    }
+}
---
+++ LESSONS/8/ClassWork8/Independentwork8/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Independentwork8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var integer = new List<int>();
+            double sum=0;
+            while (true)
+            {
+                Console.WriteLine("Введите число:");
+                var str =Console.ReadLine();
+                if (str.ToUpper()=="STOP")
+                {
+                    foreach (var item in integer)
+                    {
+                        sum += item;
+                    }
+                    Console.WriteLine(sum / integer.Count);
+                    break;
+                }
+                else
+                {
+                    integer.Add(int.Parse(str));
+                }
+               
+
+
+            }
+            
+        }
+
+        
+    }
+}
