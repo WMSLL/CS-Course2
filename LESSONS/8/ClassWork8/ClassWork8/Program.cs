0f946106f4be878acbea169041c461a0e47caa69
Кизлик виктор <62074815+WMSLL@users.noreply.github.com>
2020-04-06 20:30:36 +0300

    Independentwork8-2

--- LESSONS/8/ClassWork8/ClassWork8/Program.cs
+++ LESSONS/8/ClassWork8/ClassWork8/Program.cs
@@ -7,29 +7,34 @@ namespace ClassWork8
         static void Main(string[] args)
         {
 
+            var dictionariDD = new Dictionary<int, string>();
+            dictionariDD.Add(1, "Russia");
+            dictionariDD.Add(2, "Greate Britain");
+            dictionariDD.Add(3, "USA");
+            Console.WriteLine(string.Join(",", dictionariDD));
+            Console.WriteLine(dictionariDD);
 
-            
-            
-           // var integer = new List<int>();
-           // integer.Add(10);
-           // integer.Add(20);
-           // integer.Add(3);
-           // integer.Add(22);
-           // integer.Add(54);
-           // integer.Add(21);
-           //
-           // Console.WriteLine(string.Join(",",integer));
-           //
-           //
-           // var stri = new List<string>();
-           // stri.Add("stri");
-           // stri.Add("ss");
-           // stri.Add("sdjdn");
-           // stri.Add("sdjdn");
-           // stri.Add("sdjdn");
-           // stri.Add("sdjdn");
-           //
-           // Console.WriteLine(string.Join(",", stri));
+
+            // var integer = new List<int>();
+            // integer.Add(10);
+            // integer.Add(20);
+            // integer.Add(3);
+            // integer.Add(22);
+            // integer.Add(54);
+            // integer.Add(21);
+            //
+            // Console.WriteLine(string.Join(",",integer));
+            //
+            //
+            // var stri = new List<string>();
+            // stri.Add("stri");
+            // stri.Add("ss");
+            // stri.Add("sdjdn");
+            // stri.Add("sdjdn");
+            // stri.Add("sdjdn");
+            // stri.Add("sdjdn");
+            //
+            // Console.WriteLine(string.Join(",", stri));
         }
     }
 }
---
+++ LESSONS/8/ClassWork8/Independentwork8-2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Independentwork8_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+        }
+    }
+}
