746b05e3805fc87e4d658bd24dcbfff426505be7
Кизлик виктор <62074815+WMSLL@users.noreply.github.com>
2020-04-09 19:00:52 +0300

    HomeWork8

--- LESSONS/8/HomeWork8/HomeWork8/Program.cs
+++ LESSONS/8/HomeWork8/HomeWork8/Program.cs
@@ -1,15 +1,50 @@
 ﻿using System;
 using System.Collections.Generic;
 
+
 namespace HomeWork8
 {
     class Program
     {
         static void Main(string[] args)
-        { 
-           
+        {
+            while (true)
+            {     
+                Console.WriteLine("Введите набор скобок [],() для выхода наберите EXIT");
+                var line =Console.ReadLine();
+                if (line.ToUpper() == "EXIT")
+                {
+                    break;
+                }
+                Console.WriteLine(CheckPltBrts(line));
+            }            
         }
 
-        
+        static bool CheckPltBrts(string value)
+        {           
+            var checkStack = new Stack<char>();
+            foreach (var brackets in value)
+            {
+                switch (brackets)
+                {
+                    case '[':
+                    case '(':
+                        checkStack.Push(brackets);
+                        break;
+                    case ']':
+                        if (checkStack.Count == 0) return false;
+                        if (checkStack.Pop() != '[') return false;
+                        break;
+                    case ')':
+                        if (checkStack.Count == 0) return false;
+                        if (checkStack.Pop() != '(') return false;
+                        break;
+                    default:
+                        if (checkStack.Count == 0) return false;
+                        break;
+                }
+            }
+            return checkStack.Count == 0;
+        }
     }
 }
