b5f3378c54e672b31ca8f85e8895e6b0ae8637f7
Кизлик виктор <62074815+WMSLL@users.noreply.github.com>
2020-04-09 18:30:39 +0300

    ClassWork8

--- LESSONS/8/HomeWork8/HomeWork8/Program.cs
+++ LESSONS/8/HomeWork8/HomeWork8/Program.cs
@@ -7,39 +7,9 @@ namespace HomeWork8
     {
         static void Main(string[] args)
         { 
-            Console.WriteLine("Введите набор скобок {},[],() ");
-            Console.WriteLine(InputStack(Console.ReadLine()));
-            Console.WriteLine("Нажмите любую клавишу.");
-            Console.ReadKey();
+           
         }
 
-        static bool InputStack(string s)
-        {
-            var stack = new Stack<char>();           
-            foreach (var c in s)
-            {
-                switch (c)
-                {
-                    case '{':
-                    case '(':
-                    case '[':
-                        stack.Push(c);                      
-                        break;
-                    case '}':
-                        if (stack.Count == 0) return false;
-                        if (stack.Pop() != '{') return false;
-                        break;
-                    case ']':
-                        if (stack.Count == 0) return false;
-                        if (stack.Pop() != '[') return false;
-                        break;
-                    case ')':
-                        if (stack.Count == 0) return false;
-                        if (stack.Pop() != '(') return false;
-                        break;
-                }
-            }
-            return stack.Count == 0;
-        }       
+        
     }
 }
