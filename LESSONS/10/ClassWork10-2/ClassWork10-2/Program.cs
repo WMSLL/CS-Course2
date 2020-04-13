96034bc872abba9f52413446e5b84cb1b85c57d5
Кизлик виктор <62074815+WMSLL@users.noreply.github.com>
2020-04-13 20:42:27 +0300

    ClassWork10-2

--- LESSONS/10/ClassWork10-2/ClassWork10-2/Program.cs
+++ LESSONS/10/ClassWork10-2/ClassWork10-2/Program.cs
@@ -11,19 +11,13 @@ namespace ClassWork10_2
                 _age = 12,
                 _kind = "Dog",
                 _name = "bob",
-                _sex = 'M' 
+                _sex = 'M'
             };
             var Pet2 = new Pet();
             Pet2._age = 12;
             Pet2._kind = "Dog";
             Pet2._name = "bob";
             Pet2._sex = 'M';
-
-
-
-
-
-
         }
 
 
