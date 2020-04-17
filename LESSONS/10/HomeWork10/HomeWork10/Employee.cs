c3889327579d55198d0ae0e6a2966e38eea8cd70
Кизлик виктор <62074815+WMSLL@users.noreply.github.com>
2020-04-17 19:50:42 +0300

    HomeWork-10

---
+++ LESSONS/10/HomeWork10/HomeWork10/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace HomeWork10
+{
+    public class Employee
+    {
+        public string[] name;
+        public int[] age;
+        static int Age4Years { get; set; }
+        static string EndLine { get; set; }        
+        public void OutPut()
+        {
+            for (int i = 0; i < age.Length; i++)
+            {
+                Age4Years = age[i] + 4;
+                EndLine = $"Name: {name[i]}, age in 4 years :{Age4Years}";
+                Console.WriteLine(EndLine);
+            }
+        }
+    }
+}
--- LESSONS/10/HomeWork10/HomeWork10/Program.cs
+++ LESSONS/10/HomeWork10/HomeWork10/Program.cs
@@ -5,9 +5,25 @@ namespace HomeWork10
     class Program
     {
         static void Main(string[] args)
-        {
-            InfoEmployee.
+        {   
+            var emp = new Employee();
+            string[] names = new string[3];
+            int[] age = new int[3];
+            for (var i = 0; i < names.Length; i++)
+            {
+                Console.WriteLine($"Enter the name of the {i + 1}st employee");
+                names[i] = Console.ReadLine();
+            }
+            for (var i = 0; i < age.Length; i++)
+            {
+                Console.WriteLine($"Enter the employee's {names[i]} age ");
+                age[i] = int.Parse(Console.ReadLine());
+            }     
+            emp.Name = names;
+            emp.age = age;
+            emp.OutPut();
+            Console.WriteLine("Press any key");
+            Console.ReadLine();
         }
-
     }
 }
