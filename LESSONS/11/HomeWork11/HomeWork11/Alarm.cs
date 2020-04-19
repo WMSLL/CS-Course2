1db2386f4974b3147b6870094b594c7376b8d55c
Кизлик виктор <62074815+WMSLL@users.noreply.github.com>
2020-04-19 11:43:36 +0300

    HomeWork-11-1

--- LESSONS/11/HomeWork11/HomeWork11/Alarm.cs
+++ LESSONS/11/HomeWork11/HomeWork11/Alarm.cs
@@ -3,12 +3,24 @@ namespace HomeWork11
 {
     class Alarm
     {
-        public DateTimeOffset AlarmDate;
-        public string AlarmMessage;
-        TimeSpan _timeToAlarm;
-        public TimeSpan TimeToAlarm { get; }
-        bool _isOutdated;
-
-        public bool IsOutdated { get; }
+        DateTimeOffset AlarmDate;
+        string AlarmMessage;
+        public TimeSpan TimeToAlarm { get { return AlarmDate - DateTimeOffset.Now; } }
+        public bool IsOutdated
+        {
+            get
+            {
+                return TimeToAlarm.TotalMinutes > 0;
+            }
+        }
+        public Alarm(DateTimeOffset date, string message)
+        {
+            AlarmDate = date;
+            AlarmMessage = message;
+        }
+        public string WriteProperties()
+        {
+            return $@"Время срабатывания будильника: {AlarmDate.ToString("g")} ,AlarmMessage:{AlarmMessage} , TimeToAlarm:{Math.Floor(TimeToAlarm.TotalMinutes / 60)}h.{Math.Floor(TimeToAlarm.TotalMinutes % 60)}m., isOutdated: {IsOutdated}";
+        }
     }
 }
