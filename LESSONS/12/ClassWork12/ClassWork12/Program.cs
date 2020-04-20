55016ed430941394ebd3bbd992cd2df7b2a6c2e9
Кизлик виктор <62074815+WMSLL@users.noreply.github.com>
2020-04-20 21:24:48 +0300

    ClassWork-12-5

--- LESSONS/12/ClassWork12/ClassWork12/Program.cs
+++ LESSONS/12/ClassWork12/ClassWork12/Program.cs
@@ -8,9 +8,11 @@ namespace ClassWork12
         public string Number;
         public DateTimeOffset IssueDate;
         public virtual string Discription => $"Title:{Title},Number:{Number}";
-        
-        public BaseDoc(string Title, string Number, DateTimeOffset IssueDate)
+
+        public BaseDoc(string title, string number)
         {
+            Number = number;
+            Title = title;
 
         }
         public void WriteToConsole()
@@ -19,22 +21,27 @@ namespace ClassWork12
         }
     }
 
-    public class Passport(string title,string number, DateTimeOffset issueDate) : BaseDoc Base(string title, string number, DateTimeOffset issueDate))
+     class Passport : BaseDoc
 
     {
+        public Passport(string nubmer,string Title) : base("21313", "passport")
+        {
+
+        }
+
         public string Country;
         public string PersonName;
         public override string Discription => $"Title:{Title},Number:{Number},Country{Country},{PersonName}";
-       
+
     }
     class Program
     {
         static void Main(string[] args)
         {
-            var pasport = new Passport();
-           pasport.WriteToConsole();
+            var pasport = new Passport("Passport", "123123");
+            pasport.WriteToConsole();
 
-            var person = new BaseDoc();
+            var person = new BaseDoc("Passport", "123123");
             person.WriteToConsole();
         }
     }
