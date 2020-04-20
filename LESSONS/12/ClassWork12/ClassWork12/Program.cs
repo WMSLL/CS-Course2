de095a75d5d185970851e3b3c14f9b0234217061
Кизлик виктор <62074815+WMSLL@users.noreply.github.com>
2020-04-20 19:51:24 +0300

    ClassWork-12-0

---
+++ LESSONS/12/ClassWork12/ClassWork12/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ClassWork12
+{
+    public class BaseDoc
+    {
+        public string Title;
+        public string Number;
+        public DateTimeOffset IssueDate;
+        public string Discription => $"Title:{Title},Number:{Number}";
+        
+        public void WriteToConsole()
+        {
+            Console.WriteLine(Discription);
+        }
+    }
+
+    public class Passport : BaseDoc
+    {
+        public string Country;
+        public string PersonName;
+        public new string Discription => $"Title:{Title},Number:{Number},Country{Country},{PersonName}";
+        public  new void  WriteToConsole()
+        {
+             Console.WriteLine(Discription);
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Passport.WriteToConsole();
+        }
+    }
+}
