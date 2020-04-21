cf7b37b85336c6e1425f44b5b35e59ead7b4c681
Кизлик виктор <62074815+WMSLL@users.noreply.github.com>
2020-04-21 17:33:00 +0300

    Independent12-1

--- LESSONS/12/ClassWork12/ClassWork12/Program.cs
+++ LESSONS/12/ClassWork12/ClassWork12/Program.cs
@@ -2,47 +2,65 @@
 
 namespace ClassWork12
 {
-    public class BaseDoc
-    {
-        public string Title;
-        public string Number;
-        public DateTimeOffset IssueDate;
-        public virtual string Discription => $"Title:{Title},Number:{Number}";
 
-        public BaseDoc(string title, string number)
-        {
-            Number = number;
-            Title = title;
-
-        }
-        public void WriteToConsole()
-        {
-            Console.WriteLine(Discription);
-        }
-    }
-
-     class Passport : BaseDoc
-
-    {
-        public Passport(string nubmer,string Title) : base("21313", "passport")
-        {
-
-        }
-
-        public string Country;
-        public string PersonName;
-        public override string Discription => $"Title:{Title},Number:{Number},Country{Country},{PersonName}";
-
-    }
     class Program
     {
         static void Main(string[] args)
         {
-            var pasport = new Passport("Passport", "123123");
-            pasport.WriteToConsole();
 
-            var person = new BaseDoc("Passport", "123123");
-            person.WriteToConsole();
+            BaseDoc[] docs = new[]
+                {
+                new Passport("123123",DateTime.Parse("20.12.1980")),
+                 new BaseDoc("DriverCertification","12312323"),
+                  new Passport("123123",DateTime.Parse("20.12.1980")),
+                  new BaseDoc("DriverCertification","2123ds231"),
+                };
+
+            foreach (var docType in docs)
+            {
+                if (docType is Passport)
+                {
+                    docType.ChangeIssueDate();
+                }
+                docType.WriteToConsole();
+            }
+        }
+
+        public class BaseDoc
+        {
+            public string Title;
+            public string Number;
+            public DateTimeOffset IssueDate;
+            public virtual string Discription => $"Title : {Title},Number : {Number},IssueDate : {IssueDate}";
+            public BaseDoc(string title, string number)
+            {
+                Number = number;
+                Title = title;
+            }
+            public void WriteToConsole()
+            {
+                Console.WriteLine(Discription);
+            }
+            public virtual void ChangeIssueDate()
+            {
+                IssueDate = DateTimeOffset.Now;
+            }
+        }
+
+        class Passport : BaseDoc
+
+        {
+            public Passport(string number, DateTimeOffset issueDate) : base("passport", number)
+            {
+
+            }
+            public string Country = "Russia";
+            public string PersonName = "Valera";
+            public override void ChangeIssueDate()
+            {
+                IssueDate = DateTimeOffset.Now;
+            }
+            public override string Discription => $"Title:{Title},Number:{Number},Country:{Country},PersonName:{PersonName},IssueDate:{IssueDate}";
         }
     }
 }
