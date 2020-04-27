434a2b535166b606b75be835e62bf8b74e717a54
Кизлик виктор <62074815+WMSLL@users.noreply.github.com>
2020-04-27 19:49:34 +0300

    Homework14 -1

---
+++ LESSONS/14/ClassWork14/ClassWork14/ErrorList.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClassWork14
+{
+    class ErrorList : IDisposable
+    {
+        public string Category { get; }
+        public List<string> Error;
+
+        public ErrorList(string Category,List<string> Error)
+        {
+
+        }
+        public void Dispose()
+        {
+            Error.Clear();
+            Error = null;
+
+        }        
+    }
+}
---
+++ LESSONS/14/ClassWork14/ClassWork14/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+namespace ClassWork14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            using (var errors = new ErrorList( "Warning",  new List<string> { "xth", "zfs", "qwe"} ))
+            {
+                foreach (var item in errors.Error)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+        }
+    }
+}
