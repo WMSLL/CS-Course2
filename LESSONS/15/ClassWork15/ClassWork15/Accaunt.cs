a44435f6bad52475c203d39d1601421d8c127c1e
Кизлик виктор <62074815+WMSLL@users.noreply.github.com>
2020-04-29 20:08:01 +0300

    ClassWork15-1

---
+++ LESSONS/15/ClassWork15/ClassWork15/Accaunt.cs
@@ -0,0 +1,19 @@
+﻿namespace ClassWork15
+{
+    class Accaunt<T>
+    {
+        public T Id { get; private set; }
+        public string Name { get; private set; }
+
+        public Accaunt(T id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+        public void WriteProperties()
+        {
+            System.Console.WriteLine($"Id:{Id}  ,   Name:{Name}");
+        }
+
+    }
+}
---
+++ LESSONS/15/ClassWork15/ClassWork15/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ClassWork15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var q1 = new Accaunt<int>(id: 1,name: "nfm");
+
+            q1.WriteProperties();
+
+            var q2 = new Accaunt<string>(id: "ssss", name: "nwerwerfm");
+
+            q2.WriteProperties();
+
+            var q3 = new Accaunt<Guid>(id: Guid.NewGuid(), name: "werewr");
+
+            q3.WriteProperties();
+
+        }
+    }
+}
