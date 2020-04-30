65629f19b7e9e2d83b53818643be38b467b2339a
Кизлик виктор <62074815+WMSLL@users.noreply.github.com>
2020-04-30 20:56:11 +0300

    ClassWork15-2

---
+++ LESSONS/15/ClassWork15/ClassWork15_2/Circle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ClassWork15_2
+{
+    public sealed class Circle
+    {
+        private double _radius;
+
+        public Circle(double radius)
+        {
+            _radius = radius;
+        }
+
+        public double Calculate (Func<double,double> operation)
+        {
+            return operation(_radius);
+        }
+    }
+}
---
+++ LESSONS/15/ClassWork15/ClassWork15_2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ClassWork15_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var c = new Circle(1.5);
+
+           
+;           
+             double aa(double arg)
+            {
+                return arg;
+            }
+
+            Console.WriteLine(c.Calculate(aa));
+
+
+
+
+
+        }
+    }
+}
