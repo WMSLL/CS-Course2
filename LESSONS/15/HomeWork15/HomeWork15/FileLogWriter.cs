54b93c0ab7ab8a1cfa008ae947d2dfb3301c10f9
Кизлик виктор <62074815+WMSLL@users.noreply.github.com>
2020-05-06 16:39:39 +0300

    HomeWork15-1

--- LESSONS/15/HomeWork15/HomeWork15/FileLogWriter.cs
+++ LESSONS/15/HomeWork15/HomeWork15/FileLogWriter.cs
@@ -12,17 +12,17 @@ namespace HomeWork13
         public void LogInfo(string message)
         {
             var error = $"{DateTimeOffset.Now}/tInfo{message}";
-            File.AppendAllText(_failNmae, error);
+            File.AppendAllText(_failNmae, error + Environment.NewLine);
         }
         public void LogWarning(string message)
         {
             var error = $"{DateTimeOffset.Now}/tWarning{message}";
-            File.AppendAllText(_failNmae, error);
+            File.AppendAllText(_failNmae, error + Environment.NewLine);
         }
         public void LogError(string message)
         {
             var error = $"{DateTimeOffset.Now}/tError{message}";
-            File.AppendAllText(_failNmae, error);
+            File.AppendAllText(_failNmae, error + Environment.NewLine);
         }
     }
 }
