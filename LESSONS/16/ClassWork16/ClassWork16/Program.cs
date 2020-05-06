e9d37ed3918ccae8f341a008d70c7fd7b009c235
Кизлик виктор <62074815+WMSLL@users.noreply.github.com>
2020-05-06 20:52:16 +0300

    ClassWork-16

---
+++ LESSONS/16/ClassWork16/ClassWork16/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using CircleStandart;
+
+namespace ClassWork16
+{
+    
+    class Program
+    { 
+        static void Main(string[] args)
+        {
+            var n = new CircleClass(2.5);
+            Console.WriteLine(n.Calculate(r => Math.PI * Math.Pow(r, 2)));
+
+        }
+    }
+}
