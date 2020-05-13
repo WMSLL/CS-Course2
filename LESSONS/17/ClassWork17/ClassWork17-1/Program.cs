57f90ea079c478b746087782c4d24806cf8bcd14
WMSLL <62074815+WMSLL@users.noreply.github.com>
2020-05-13 21:43:43 +0300

    ClassWork17

---
+++ LESSONS/17/ClassWork17/ClassWork17-1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+
+namespace ClassWork17_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {         
+            
+            var gen = new Generate();
+            gen.GenAct += Printing;
+            gen.RandomGenNum(6);
+        }
+
+        public static void Printing(byte[] arg)
+        {
+            foreach (var item in arg)
+            {
+                File.AppendAllText("log.txt",item.ToString());
+            }
+        }
+    }
+
+    class GeneratedateEvent
+    {
+
+
+    }
+    class Generate
+    {
+
+        public event Action<byte[]> GenAct;
+        public void RandomGenNum(int counts)
+        {
+            var random = new Random();
+            var array = new byte[counts];
+            random.NextBytes(array);
+            GenAct.Invoke(array);
+        }
+
+    }
+}
+
---
+++ LESSONS/17/ClassWork17/ClassWork17/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading;
+
+namespace ClassWork17
+{
+    enum Task
+    {
+        Standart,Complex
+    }
+    class Program
+    {
+        delegate void WorkHandel (Task task, int seconds);
+        static void Main(string[] args)
+        {
+            WorkHandel d = workOne;
+            d += workTwo;
+            d += workThree;
+        }
+
+        private static void workOne(Task task, int seconds)
+        {
+            Thread.Sleep(seconds);
+            Console.WriteLine($" workOne {task} {seconds}");
+        }
+
+        private static void workTwo(Task task, int seconds)
+        {
+            Thread.Sleep(seconds);
+            Console.WriteLine($" workTwo {task} {seconds}");
+        }
+
+        private static void workThree(Task task, int seconds)
+        {
+            Thread.Sleep(seconds);
+            Console.WriteLine($"workThree {task} {seconds}");
+        }
+    }
+}
