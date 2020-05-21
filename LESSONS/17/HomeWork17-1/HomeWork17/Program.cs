ba96ecd77d5769ddaccf7ce0cb23251b2de1866c
WMSLL <62074815+WMSLL@users.noreply.github.com>
2020-05-21 16:08:09 +0300

    45

--- LESSONS/17/HomeWork17-1/HomeWork17/Program.cs
+++ LESSONS/17/HomeWork17-1/HomeWork17/Program.cs
@@ -6,7 +6,25 @@ namespace HomeWork17
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var writer1 = new FileWriterWithProgress();
+            writer1.GenAct += OnStatrt;
+            var data = new byte[100];
+            var random = new Random();
+            random.NextBytes(data);
+
+            OnStatrt(data);
+            for (int i = 0; i <= data.GetUpperBound(0); i++)
+            {
+                Console.WriteLine("{0}: {1}", i, data[i]);
+            }            
+        }
+
+        private static void OnStatrt(byte[] obj)
+        {
+            for (int i = 0; i <= obj.GetUpperBound(0); i++)
+            {
+                Console.WriteLine("{0}: {1}", i, obj[i]);
+            }
         }
     }
 }
