f0f97acb641734fe37acea30ab562f6ab54fab96
WMSLL <62074815+WMSLL@users.noreply.github.com>
2020-05-22 20:30:06 +0300

    Rimander

--- LESSONS/Reminder/Reminder.Storege.Memory/ReminderItemStorege.cs
+++ LESSONS/Reminder/Reminder.Storege.Memory/ReminderItemStorege.cs
@@ -5,20 +5,12 @@ namespace Reminder.Storage.Memory
     public class ReminderItemStorege : IReminderItem
     {
         private Dictionary<Guid, ReminderItem> _item;
-
-        public ReminderItemStorege()
-        {
-        }
-
         public ReminderItemStorege(Dictionary<Guid, ReminderItem> item)
         {
             _item = item;
         }
-        //
-        //
         public void Add(ReminderItem item)
         {
-          
             if (item == null)
             {
                 throw new ArgumentNullException(nameof(item));
@@ -26,35 +18,68 @@ namespace Reminder.Storage.Memory
 
             if (_item.ContainsKey(item.id))
             {
-                throw new ArgumentException("Reminder item with key already exists",nameof(item));
+                throw new ArgumentException("Reminder item with key already exists", nameof(item));
             }
-            var key = item.id; 
+            var key = item.id;
             _item.Add(key, item);
-
-        }   
-
-        public void Delete(Guid id)
-        {           
         }
 
+        public void Delete(Guid id)
+        {        // На удаление думаю и теста не нужно.    
+            _item.Remove(id);
+        }
         public ReminderItem Find(Guid id)
         {
-            throw new NotImplementedException();
+            _item.TryGetValue(id, out ReminderItem value);
+            return value;
         }
 
         public ReminderItem[] FindByDateTime(DateTimeOffset dateTime)
         {
-            throw new NotImplementedException();
+            var reminderValue = _item.Values;
+            var remainderList = new List<ReminderItem>();
+            foreach (var item in reminderValue)
+            {
+                if (item.DateTimeUtc <= dateTime)
+                {
+                    remainderList.Add(item);
+                }
+            }
+            var countList = remainderList.Count;
+            var remainderArray = new ReminderItem[countList];
+            for (int i = 0; i < countList; i++)
+            {
+                remainderArray[i] = remainderList[i];
+            }
+            return remainderArray;
         }
 
         public ReminderItem[] FindByStatus(ReminderItem.ReminderItemStatus status)
         {
-            throw new NotImplementedException();
+            var remainderList = new List<ReminderItem>();
+            foreach (var item in _item.Values)
+            {
+                if (item.Status == status)
+                {
+                    remainderList.Add(item);
+                }
+            }
+            var countList = remainderList.Count;
+            var remainderArray = new ReminderItem[countList];
+            for (int i = 0; i < countList; i++)
+            {
+                remainderArray[i] = remainderList[i];
+            }
+            return remainderArray;
         }
 
         public void Update(ReminderItem item)
         {
-           
+            if (item == null)
+            {
+                throw new ArgumentNullException(nameof(item));
+            }
+            _item[item.id] = item;
         }
     }
 }
