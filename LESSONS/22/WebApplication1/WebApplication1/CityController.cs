4568cb6030563914f5ea4e2577f758191c789759
WMSLL <62074815+WMSLL@users.noreply.github.com>
2020-06-03 18:24:52 +0300

    12wesdf

---
+++ LESSONS/22/WebApplication1/WebApplication1/CityController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace WebApplication1
+{
+    public class City
+    { 
+        public string Title { get; set; }
+        public Guid Id { get; set; }
+        public DateTimeOffset DateTime { get; set; }
+        public int Population { get; set; }
+
+        public City(string title, DateTimeOffset dateTime, int population)
+        {
+            Title = title;
+            DateTime = dateTime;
+            Population = population;
+            Id = Guid.NewGuid();
+        }
+
+    }
+    public class CityController : ControllerBase
+    {
+        public IActionResult List()
+        {
+            throw Exception();
+        }
+    }
+}
