413ffcb2ab17014e40eaec0460fc8def11967bb6
WMSLL <62074815+WMSLL@users.noreply.github.com>
2020-06-08 19:02:49 +0300

    sdad

---
+++ LESSONS/23/WebApplication2/WEBAPI/Program.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Hosting;
+using System;
+
+namespace WEBAPI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHost(o => o.UseStartup<Startup>())
+                .Build()
+                .Run();
+                
+        }
+    }
+
+    internal class Startup
+    {
+        private void Configure()
+        {
+
+        }
+    }
+}
