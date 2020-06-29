a3a41fb453eb726ab825d09969891ab8b4318cd5
WMSLL <62074815+WMSLL@users.noreply.github.com>
2020-06-29 20:57:53 +0300

    Update Mari.sql

--- LESSONS/SQL/Mari.sql
+++ LESSONS/SQL/Mari.sql
@@ -1,7 +1,12 @@
 
 
-Select sum(Count*p.price)
+ Select  id,name,sum(total)
+ From (
+Select  c.id,c.name,(1- isnull(Discount,0))*sum(l.count*p.price) total
 From customer c join [Order] o on o.customerid=c.id
                 join orderline l on l.orderid=o.id
                 join product p on p.id=l.productid
-where c.name=N'Мария'
+                Group by  c.id,c.name,isnull(Discount,0)
+ )  t
+
+ Group by id,name
