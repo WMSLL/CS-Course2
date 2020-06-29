0749f7525e185d0ba511bd437526f866e51d7642
WMSLL <62074815+WMSLL@users.noreply.github.com>
2020-06-29 13:53:12 +0300

    Update ReminderItemStorage.sql

--- LESSONS/SQL/ReminderItemStorage.sql
+++ LESSONS/SQL/ReminderItemStorage.sql
@@ -1,3 +1,9 @@
+
+Create table StatusMessage (ID int primary key identity,Status nvarchar(25))
+
+Create table UserProfile( ID int primary key identity,User_name nvarchar(50))
+
+
 Create table ReminderItemStorageSQL (
 Id uniqueidentifier not null Primary Key
 ,Title nvarchar(256)not null
@@ -9,9 +15,5 @@ Id uniqueidentifier not null Primary Key
 ,   FOREIGN KEY(UserId) REFERENCES UserProfile(ID)
 ,   FOREIGN KEY(STATUS) REFERENCES StatusMessage(ID)  )
 
-Create table StatusMessage (ID int primary key identity,Status nvarchar(25)
-,UserDef1 nvarchar(50),UserDef2 nvarchar(50)
-,UserDef3 nvarchar(50),UserDef4 nvarchar(50) ,UserDef5 numeric(19,5),UserDef6 numeric(19,5) )
 
-Create table UserProfile( ID int primary key identity,User_name nvarchar(50),UserDef1 nvarchar(50),UserDef2 nvarchar(50)
-,UserDef3 nvarchar(50),UserDef4 nvarchar(50) ,UserDef5 numeric(19,5),UserDef6 numeric(19,5) )
+
