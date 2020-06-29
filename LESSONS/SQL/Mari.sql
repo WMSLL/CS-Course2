c3d0cbadd27b3eda0f2e8b35914a833391b99ce8
WMSLL <62074815+WMSLL@users.noreply.github.com>
2020-06-29 20:59:33 +0300

    Update Mari.sql

--- LESSONS/SQL/Mari.sql
+++ LESSONS/SQL/Mari.sql
@@ -1,12 +1,12 @@
 
 
- Select  id,name,sum(total)
+ Select  id,name,sum(total),YEAR
  From (
-Select  c.id,c.name,(1- isnull(Discount,0))*sum(l.count*p.price) total
+Select  c.id,c.name,(1- isnull(Discount,0))*sum(l.count*p.price) total,YEAR(Orderdate) [YEAR]
 From customer c join [Order] o on o.customerid=c.id
                 join orderline l on l.orderid=o.id
                 join product p on p.id=l.productid
-                Group by  c.id,c.name,isnull(Discount,0)
+                Group by  c.id,c.name,isnull(Discount,0),YEAR(Orderdate) 
  )  t
-
- Group by id,name
+ Group by id,name,YEAR
+ order by name,YEAR
