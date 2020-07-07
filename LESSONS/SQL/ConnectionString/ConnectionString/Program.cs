c0e85845a50400690f0a5948a6f7ce5fbd1255ef
WMSLL <62074815+WMSLL@users.noreply.github.com>
2020-07-07 08:32:13 +0300

    ConnectionString

---
+++ LESSONS/SQL/ConnectionString/ConnectionString/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.Data.SqlClient;
+namespace ConnectionString
+{
+    class Program
+    {
+
+        private const string ConnectionStrong=
+        "Server=tcp:shadow-art.database.windows.net,1433;" +
+        "Initial Catalog=reminder; " +
+        "Persist Security Info=False;" +
+        "User ID=v_kizlik@shadow-art;" +
+        "Password=Y6t8mO03d5cksKGQPoP8;" +
+        "Encrypt=True;";
+        static void Main(string[] args)
+        {
+            using var connect = new SqlConnection(ConnectionStrong);
+            
+        }
+    }
+}
--- LESSONS/SQL/Mari.sql
+++ LESSONS/SQL/Mari.sql
@@ -1,12 +1,10 @@
 
 
- Select  id,name,sum(total),YEAR
- From (
-Select  c.id,c.name,(1- isnull(Discount,0))*sum(l.count*p.price) total,YEAR(Orderdate) [YEAR]
-From customer c join [Order] o on o.customerid=c.id
-                join orderline l on l.orderid=o.id
-                join product p on p.id=l.productid
-                Group by  c.id,c.name,isnull(Discount,0),YEAR(Orderdate) 
+Select  sum(total),YEAR
+From (
+Select  (1- isnull(Discount,0))*sum(l.count*p.price) total,YEAR(Orderdate) [YEAR]
+From  [Order] o  join orderline l on l.orderid=o.id
+                 join product p on p.id=l.productid
+                 Group by  isnull(Discount,0),YEAR(Orderdate) 
  )  t
- Group by id,name,YEAR
- order by name,YEAR
+Group by YEAR
